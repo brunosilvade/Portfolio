efbd49d00016f2981896ba51ceee095e8bb0095c
Bruno Almeida <brunosilvade@gmail.com>
2026-02-28 16:47:58 -0300

    feat: setup router structure with layout and home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-export function App() {
-  return (
-    <div>
-      <h1>Portfolio Bruno Almeida</h1>
-    </div>
-  );
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
+
+function App() {
+  return <RouterProvider router={router} />;
 }
+
+export default App;
