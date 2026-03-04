75527dd75ceb47aa33fb781d9d49db1c8a46b7e6
Bruno Almeida <brunosilvade@gmail.com>
2026-03-04 00:09:07 -0300

    chore(styles): create variables.scss and update all scss files to use variables tokens

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -5,40 +5,47 @@ export const Contact = () => {
   return (
     <section className="contact">
       <div className="container">
-      <h2>Contato</h2>
+        <h2>Contato</h2>
 
-      <div className="contact-card">
-        <form action="mailto:seuemail@email.com" method="POST" encType="text/plain">
-          <input type="text" name="name" placeholder="Seu nome" required />
-          <input type="email" name="email" placeholder="Seu email" required />
-          <textarea name="message" rows={5} placeholder="Sua mensagem" required />
-          <button type="submit">Enviar mensagem</button>
-        </form>
-      </div>
+        <div className="contact-card">
+          <form
+            action="mailto:seuemail@email.com"
+            method="POST"
+            encType="text/plain"
+          >
+            <input type="text" name="name" placeholder="Seu nome" required />
+            <input type="email" name="email" placeholder="Seu email" required />
+            <textarea
+              name="message"
+              rows={5}
+              placeholder="Sua mensagem"
+              required
+            />
+            <button type="submit">Enviar mensagem</button>
+          </form>
 
-      <p className="contact-alt">ou me encontre em:</p>
+          <p className="contact-alt">ou me encontre em:</p>
 
-      <div className="contact-social">
-        <a
-          href="https://www.linkedin.com/in/brunosilvadealmeida/"
-          target="_blank"
-          rel="noreferrer"
-          className="social-btn"
-        >
-          <FaLinkedin />
-          LinkedIn
-        </a>
+          <div className="contact-social">
+            <a
+              href="https://www.linkedin.com/"
+              target="_blank"
+              rel="noreferrer"
+              className="social-linkedin"
+            >
+              <FaLinkedin /> LinkedIn
+            </a>
 
-        <a
-          href="https://github.com/brunosilvade"
-          target="_blank"
-          rel="noreferrer"
-          className="social-btn"
-        >
-          <FaGithub />
-          GitHub
-        </a>
-      </div>
+            <a
+              href="https://github.com/"
+              target="_blank"
+              rel="noreferrer"
+              className="social-github"
+            >
+              <FaGithub /> GitHub
+            </a>
+          </div>
+        </div>
       </div>
     </section>
   );
